1f9491f76e6828055ded107cde151db5b7b554b9
Evan Lundell <elundell@titancloud.com>
2021-01-29 13:37:24 -0600

    added polls

---
+++ src/Brobot.Commands/Models/PollEmoji.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Brobot.Commands.Models
+{
+    internal static class PollEmoji
+    {
+        public static Dictionary<int, string> DiscordCode = new Dictionary<int, string>
+        {
+            { 1, ":one:" },
+            { 2, ":two:" },
+            { 3, ":three:" },
+            { 4, ":four:" },
+            { 5, ":five:" },
+            { 6, ":six:" },
+            { 7, ":seven:" },
+            { 8, ":eight:" },
+            { 9, ":nine:" },
+            { 10, ":keycap_ten:" }
+        };
+
+        public static Dictionary<int, string> Unicode = new Dictionary<int, string>
+        {
+            { 1, "1️⃣" },
+            { 2, "2️⃣" },
+            { 3, "3️⃣" },
+            { 4, "4️⃣" },
+            { 5, "5️⃣" },
+            { 6, "6️⃣" },
+            { 7, "7️⃣" },
+            { 8, "8️⃣" },
+            { 9, "9️⃣" },
+            { 10, "🔟" }
+        };
+    }
+}
---
+++ src/Brobot.Commands/Modules/PollModule.cs
@@ -0,0 +1,56 @@
+﻿using Brobot.Commands.Models;
+using Discord;
+using Discord.Commands;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Brobot.Commands.Modules
+{
+    public class PollModule : ModuleBase<SocketCommandContext>
+    {
+        [Command("poll")]
+        [Summary("Creates a poll. Ex: !poll \"Who is your favorite super hero?\" \"Superman\" \"Spider-Man\" \"Wonder Woman\"")]
+        public async Task Poll(params string[] options)
+        {
+            if (options.Length == 0)
+            {
+                await ReplyAsync("Provide a question and a list of options");
+                return;
+            }
+
+            if (options.Length < 3)
+            {
+                await ReplyAsync("Provide a question and at least two options");
+                return;
+            }
+
+            if (options.Length > 11)
+            {
+                await ReplyAsync("Only 10 options are allowed");
+                return;
+            }
+
+            var question = options[0];
+            var answers = options.Skip(1).Take(options.Length - 1).ToArray();
+
+            var response = new StringBuilder($"**{question}**\n\n");
+            for (int i = 0; i < answers.Length; i++)
+            {
+                response.AppendLine($"{PollEmoji.DiscordCode[i + 1]} {answers[i]}");
+            }
+
+            var message = await ReplyAsync(response.ToString());
+            var reactions = new IEmote[answers.Length];
+
+            for (int i = 0; i < answers.Length; i++)
+            {
+                reactions[i] = new Emoji(PollEmoji.Unicode[i + 1]);
+            }
+
+            await message.AddReactionsAsync(reactions);
+        }
+    }
+}
